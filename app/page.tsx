e3d9599c49e7a570b282a56f5d151f45e1976b24
atefe-fazeli <atefefazeli1996@gmail.com>
2025-05-08 10:38:18 +0330

    type anotation and inference

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   let number3 = 15;
   // number3="ali"
-  // number3 has number type by default
+  // number3 has number type by default(by first value) => type inference
 
   let num5: number = 67;
   // num5="ali"
@@ -19,7 +19,9 @@ export default function Home() {
 
   let str1: string = "bochi";
   // str1=12
-  //  str1 has string type
+  //  str1 has string type  => type annotation 
+
+
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
